5cde348bf3280a1f3935fa16546d56314276cc67
Ananth Vankipuram <vsap78@gmail.com>
2022-07-04 17:17:45 -0700

    adding the main.go

---
+++ internal/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+func main(){
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w, "hello, %q", html.EscapeString(r.URL.Path))
+
+	})
+
+	log.Println("listening on localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
